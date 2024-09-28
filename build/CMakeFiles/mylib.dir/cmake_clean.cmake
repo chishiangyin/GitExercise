1c916ac5e4eb6bd8290f017e2eddf91fecacbe8b
yinqixiang <yinqixiang@lxlogin003.ihep.ac.cn>
2024-09-28 19:10:03 +0800

    add some Cmake files

---
+++ build/CMakeFiles/mylib.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mylib.dir/main.cc.o"
+  "CMakeFiles/mylib.dir/main.cc.o.d"
+  "libmylib.pdb"
+  "libmylib.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mylib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
